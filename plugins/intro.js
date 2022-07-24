662e238e9b0e63bbff25bcb100d7a27633745ce8
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-24 13:04:40 +0700

    Create intro.js

---
+++ plugins/intro.js
@@ -0,0 +1,17 @@
+let handler = async m => {
+
+let krtu = `Kartu Intro`
+m.reply(`
+0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Kartu Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Asal         :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`.trim()) // Tambah sendiri kalo mau
+}
+handler.command = /^(intro)$/i
+
+module.exports = handler
