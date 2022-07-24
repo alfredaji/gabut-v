55f47a853502d261ec34d431bf9a022960a1f3e8
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-25 00:49:27 +0700

    Update admin!.js

--- plugins/admin!.js
+++ plugins/admin!.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Jadi kan bot sebagai admin'
+  if (m.fromMe) throw 'Bot bukan admin kak'
   if (isAdmin) throw 'Padahal udah jadi admin'
   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
 }
